4555a8e0e473d58468e15b8107d5df61abe8c109
koozzi <koozzi666@gmail.com>
2021-05-16 14:53:39 +0900

    change base url

--- test/users_register.test.js
+++ test/users_register.test.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-const baseURL = "http://localhost:5000/users/register"
+const baseURL = "http://15.164.102.193:5000/users/register"
 
 test("Register new user", async () => {
     const response = await fetch(baseURL, { method: "POST" })
