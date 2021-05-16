685d3573f741501c6ce8579b7873837a72b17f67
koozzi <koozzi666@gmail.com>
2021-05-16 14:49:49 +0900

    change base url

--- test/users_login.test.js
+++ test/users_login.test.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-const baseURL = "http://15.164.102.193:5000/users/login"
+const baseURL = "http://localhost:5000/users/login"
 
 test("Login", async () => {
     const response = await fetch(baseURL, { method: "POST" })
