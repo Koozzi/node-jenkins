62c805be6202cd447c74730ac61f1fc6a25e945d
koozzi <koozzi666@gmail.com>
2021-05-17 12:40:15 +0900

    myserver.sh

--- myserver.sh
+++ myserver.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo MONGODB_CONNECTION_KEY=${MONGODB_CONNECTION_KEY} > .env
-echo JWT_SECRET=${JWT_SECRET} >> .env
-
-cat .env
 
 docker pull koozzi666/myserver
-
 docker rm -f `docker ps -aq`
 docker run -d -p 5000:5000 --env-file .env koozzi666/myserver
